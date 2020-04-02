2d5d1a62b44b763d628c1631487c7ce0e91c3e58
Alexey Stukalov <astukalov@gmail.com>
2020-04-02 02:20:58 +0200

    [*] match_protgroups.jl: cleanups, output protgrps

--- match_protgroups.jl
+++ match_protgroups.jl
@@ -1,5 +1,5 @@
 proj_info = (id = "cov2",
-             data_ver = "20200331")
+             match_ver = "20200331")
 using Pkg
 Pkg.activate(@__DIR__)
 using Revise
@@ -47,20 +47,32 @@ proteins_df[!, :ac_rank] .= [
     ifelse(r.src_db == "sp", 0, 3) +
     coalesce(r.protein_existence, 7) - 1
     for r in eachrow(proteins_df)]
-
-# dataset names ordered by priority
-ds_names = ["SKN_phospho", "ZIKV_apms", "hNPC_proteome", "candidates_apms"]
-pg_dfs = [ds_name => protgroup_acs_rdata["all_protgroups"][ds_name] for ds_name in ds_names];
 protein_ac_ranks = Dict(r.protein_ac => r.ac_rank for r in eachrow(proteins_df))
 
 # save the matches
 pg_matches_df = ProtgroupXMatch.match_protgroups(protgroups_dfs, protein_ac_ranks);
-pg_matches_df[coalesce.(pg_matches_df.gene_names, "") .== "DPYSL2",
-              [#:gene_names, :best_match_rank, :worst_match_rank,
-               Symbol("row_ix.SKN_phospho"), #Symbol("row_ix.ZIKV_apms"),
-               Symbol("row_ix.hNPC_proteome"), Symbol("row_ix.candidates_apms")]]
-CSV.write(joinpath(analysis_path, "$(project_id)_all_protgroups_matches_$(analysis_version).txt"),
-          pg_matches_df, delim='\t', null="NULL");
+for (dsname, pg_df) in protgroups_dfs
+    pg_col = Symbol("protgroup_id_", dsname)
+    rowix_col = Symbol("rowix_", dsname)
+    pg_matches_df[!, pg_col] = missings(nonmissingtype(eltype(pg_df.protgroup_id)), nrow(pg_matches_df))
+    for (i, rowix) in enumerate(pg_matches_df[!, rowix_col])
+        if !ismissing(rowix)
+            pg_matches_df[i, pg_col] = pg_df[rowix, :protgroup_id]
+        end
+    end
+    select!(pg_matches_df, Not(rowix_col))
+end
+
+nonunique_matches_dfs = [dsname => begin
+    pg_col = Symbol("protgroup_id_", dsname)
+    by(filter(r -> !ismissing(r[pg_col]), pg_matches_df), pg_col) do df
+        return nrow(df) > 1 ? df : df[1:0, :]
+    end
+end for (dsname, _) in protgroups_dfs]
+
+nonunique_matches_dfs[1][2].protgroup_id_OeProteome
+CSV.write(joinpath(analysis_path, "$(proj_info.id)_protgroup_matches_$(proj_info.match_ver).txt"),
+          pg_matches_df, delim='\t', missingstring="");
 #output_filepath = joinpath(analysis_path, "$(project_id)_all_protgroups_matches_$(analysis_version).RData");
 #@rput(output_filepath, output_filepath)
 #@rput(combined_pgs_df, combined_pgs_df)
