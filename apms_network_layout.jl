a019b65e0e9c87702f926881a55a3b2656772b88
Alexey Stukalov <astukalov@gmail.com>
2020-04-11 14:25:58 +0200

    [*] apms_net_layout: be protgroup/regroup agnostic

--- apms_network_layout.jl
+++ apms_network_layout.jl
@@ -1,7 +1,7 @@
 proj_info = (id = "cov2",
              data_ver = "20200329",
              fit_ver = "20200329",
-             model_obj = "protgroup",
+             modelobj = "protgroup",
              mq_folder = "mq_apms_20200329",
              network_ver = "20200329")
 using Pkg
@@ -17,38 +17,39 @@ const results_path = joinpath(analysis_path, "results")
 const scratch_path = joinpath(analysis_path, "scratch")
 const plots_path = joinpath(analysis_path, "plots")
 
+Revise.includet(joinpath(misc_scripts_path, "clustering_utils.jl"))
+
 network_rdata = load(joinpath(networks_path, "$(proj_info.id)_4graph_$(proj_info.mq_folder)_$(proj_info.fit_ver).RData"))
 
-protgroups_orig_df = network_rdata["protgroups_4graphml.df"]
+objects_orig_df = network_rdata["objects_4graphml.df"]
 iactions_orig_df = network_rdata["iactions_ex_4graphml.df"]
 
 nodesize_scale = 0.015
 node_type_props = Dict("bait" => (mass=2.0, width=6nodesize_scale, height=6nodesize_scale),
                        "prey" => (mass=2.0, width=3nodesize_scale, height=3nodesize_scale))
-protgroups_df = copy(protgroups_orig_df)
-protgroups_df.protgroup_id = convert(Vector{Int}, protgroups_df.protgroup_id)
-protgroups_df.mass = getproperty.(getindex.(Ref(node_type_props), protgroups_df.exp_role), :mass);
-protgroups_df.width = getproperty.(getindex.(Ref(node_type_props), protgroups_df.exp_role), :width);
-protgroups_df.height = getproperty.(getindex.(Ref(node_type_props), protgroups_df.exp_role), :height);
-sort!(protgroups_df, :protgroup_id)
+objects_df = copy(objects_orig_df)
+objects_df.mass = getproperty.(getindex.(Ref(node_type_props), objects_df.exp_role), :mass);
+objects_df.width = getproperty.(getindex.(Ref(node_type_props), objects_df.exp_role), :width);
+objects_df.height = getproperty.(getindex.(Ref(node_type_props), objects_df.exp_role), :height);
+sort!(objects_df, :object_id)
 
 ppi_weight_scales = Dict("ppi_low" => 0.05,
                          "ppi_medium" => 0.05,
                          "ppi_strong" => 0.05,
                          "complex" => 0.05,
                          "experiment" => 1.0)
-iactions_df = filter(r -> r.src_protgroup_id != r.dest_protgroup_id, iactions_orig_df)
-iactions_df.src_protgroup_id = convert(Vector{Int}, iactions_df.src_protgroup_id)
-iactions_df.dest_protgroup_id = convert(Vector{Int}, iactions_df.dest_protgroup_id)
+iactions_df = filter(r -> r.src_object_id != r.dest_object_id, iactions_orig_df)
+iactions_df.src_object_id = convert(Vector{Int}, iactions_df.src_object_id)
+iactions_df.dest_object_id = convert(Vector{Int}, iactions_df.dest_object_id)
 #filter!(r -> coalesce(r.ppi_type, "") ∉ ["enriched_complex_gocc"], iactions_df)
 iactions_df.edge_weight =
     get.(Ref(ppi_weight_scales), coalesce.(iactions_df.type, "experiment"), 0.1) .*
     iactions_df.weight
-for obj_iactions_df in groupby(iactions_df, :src_protgroup_id)
-    obj_id = obj_iactions_df.src_protgroup_id[1]
-    node_ix = searchsortedfirst(protgroups_df.protgroup_id, obj_id)
-    @assert protgroups_df.protgroup_id[node_ix] == obj_id
-    if protgroups_df.exp_role[node_ix] != "bait"
+for obj_iactions_df in groupby(iactions_df, :src_object_id)
+    obj_id = obj_iactions_df.src_object_id[1]
+    node_ix = searchsortedfirst(objects_df.object_id, obj_id)
+    @assert objects_df.object_id[node_ix] == obj_id
+    if objects_df.exp_role[node_ix] != "bait"
         #@warn "Node $obj_id is not a bait"
         continue
     end
@@ -65,23 +66,23 @@ using LightGraphs
 Revise.includet(joinpath(misc_scripts_path, "forceatlas3_layout.jl"))
 FA = ForceAtlas3
 
-gr_apms = SimpleGraph(FA.Graph(iactions_df[ismissing.(iactions_df.ppi_type), :], protgroups_df,
-                       src_node_col=:src_protgroup_id, dest_node_col=:dest_protgroup_id, weight_col=:edge_weight,
-                       node_col=:protgroup_id))
+gr_apms = SimpleGraph(FA.Graph(iactions_df[ismissing.(iactions_df.ppi_type), :], objects_df,
+                       src_node_col=:src_object_id, dest_node_col=:dest_object_id, weight_col=:edge_weight,
+                       node_col=:object_id))
 
-#protgroups_df.object_label[protgroups_df.object_label .∈ Ref(Set(["4", "9", "12", "28"]))]
-#node_dislikes_baits[protgroups_df.object_label .∈ Ref(Set(["4", "9", "12", "28"])),
-#              protgroups_df.object_label .∈ Ref(Set(["4", "9", "12", "28"]))]
+#objects_df.object_label[objects_df.object_label .∈ Ref(Set(["4", "9", "12", "28"]))]
+#node_dislikes_baits[objects_df.object_label .∈ Ref(Set(["4", "9", "12", "28"])),
+#              objects_df.object_label .∈ Ref(Set(["4", "9", "12", "28"]))]
 
 node_dislikes = FA.socioaffinity(gr_apms, p=(0.0, 1.0), q=1.0)
 node_dislikes_baits = FA.socioaffinity(gr_apms, p=(1.25, 1.25), q=2.0)
-baits_mask = protgroups_df.exp_role .== "bait"
+baits_mask = objects_df.exp_role .== "bait"
 node_dislikes[baits_mask, baits_mask] .= 4 * node_dislikes_baits[baits_mask, baits_mask]
 
-gr = FA.Graph(iactions_df, protgroups_df,
-              src_node_col=:src_protgroup_id, dest_node_col=:dest_protgroup_id, weight_col=:edge_weight,
+gr = FA.Graph(iactions_df, objects_df,
+              src_node_col=:src_object_id, dest_node_col=:dest_object_id, weight_col=:edge_weight,
               #gravitable_col=:gravitable,
-              node_col=:protgroup_id, mass_col=:mass,
+              node_col=:object_id, mass_col=:mass,
               width_col=:width, height_col=:height)
 FA.layout!(gr, FA.ForceAtlas3Settings(gr,
             outboundAttractionDistribution=false,
@@ -100,24 +101,25 @@ FA.layout!(gr, FA.ForceAtlas3Settings(gr,
             gravityRodCorners=((0.0, -4.0), (0.0, 4.0)), gravityRodCenterWeight=0.1),
             nsteps=5000, progressbar=true)
 
-protgroups_df.layout_x = FA.extract_layout(gr)[1] .* 20
-protgroups_df.layout_y = FA.extract_layout(gr)[2] .* 20
+objects_df.layout_x = FA.extract_layout(gr)[1] .* 20
+objects_df.layout_y = FA.extract_layout(gr)[2] .* 20
 
 includet(joinpath(misc_scripts_path, "graphml_writer.jl"))
 
-protgroups_df.protgroup_idstr = string.(protgroups_df.protgroup_id)
-iactions_df.src_protgroup_idstr = string.(iactions_df.src_protgroup_id)
-iactions_df.dest_protgroup_idstr = string.(iactions_df.dest_protgroup_id)
+objects_df.object_idstr = string.(objects_df.object_id)
+iactions_df.src_object_idstr = string.(iactions_df.src_object_id)
+iactions_df.dest_object_idstr = string.(iactions_df.dest_object_id)
 
-apms_graph = GraphML.import_graph(protgroups_df, iactions_df,
-                             node_col=:protgroup_idstr,
-                             source_col=:src_protgroup_idstr, target_col=:dest_protgroup_idstr,
-                             node_attrs = [:protgroup_idstr => "Protein Group ID",
-                                           :protgroup_label => "Protein Group Label",
+apms_graph = GraphML.import_graph(objects_df, iactions_df,
+                             node_col=:object_idstr,
+                             source_col=:src_object_idstr, target_col=:dest_object_idstr,
+                             node_attrs = [:object_idstr => "Protein Group ID",
+                                           :object_label => "Protein Group Label",
                                            :majority_protein_acs => "Majority ACs",
                                            :gene_names => "Gene Names",
                                            :exp_role => "Experimental Role",
-                                           :protein_names => "Protein Name",
+                                           :protein_names => "Protein Names",
+                                           :protein_description => "Protein Description",
                                            :protein_class => "Protein Class",
                                            :seqlen => "Seq Length",
                                            :is_detected => "Detected",
