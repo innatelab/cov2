94cac0a4a06c16f4afd38f841443c1ef3a250199
Alexey Stukalov <astukalov@gmail.com>
2020-04-29 00:31:52 +0200

    [*] update noise model calibration script

--- qep5_apms_calib.jl
+++ qep5_apms_calib.jl
@@ -1,6 +1,6 @@
 proj_info = (id = "cov2",
-             data_ver = "20200425",
-             fit_ver = "20200425",
+             data_ver = "20200428",
+             fit_ver = "20200428",
              modelobj = :protgroup,
              msinstrument = "QEP5",
              quantobj = :pepmodstate,
@@ -22,7 +22,7 @@ const plots_path = joinpath(analysis_path, "plots")
 
 include(joinpath(misc_scripts_path, "bbo_utils.jl"))
 include(joinpath(misc_scripts_path, "msinstrument.jl"))
-Revise.includet(joinpath(misc_scripts_path, "msinstrument_calib.jl"))
+include(joinpath(misc_scripts_path, "msinstrument_calib.jl"))
 include(joinpath(misc_scripts_path, "frame_utils.jl"))
 include(joinpath(misc_scripts_path, "delimdata_utils.jl"))
 include(joinpath(misc_scripts_path, "mq_utils.jl"))
@@ -47,9 +47,8 @@ mqevidence.observations.mschannel = copy(mqevidence.observations.msrun)
 
 mscalib_data = MSInstrumentCalibration.MSErrorCalibrationData(mqevidence, msruns_df,
                                                               object=proj_info.quantobj, intensity_col=proj_info.quantcol,
-                                                              max_objects = 10000)
-
-instr_calib_filename = "instr_$(proj_info.msinstrument)_$(proj_info.quanttype)_$(proj_info.quantobj)_calib_$(proj_info.id)_$(proj_info.data_ver)"
+                                                              allowed_ident_types = ["ISO-MSMS", "MULTI-MSMS"],
+                                                              max_objects = 3000)
 
 using JLD2, JSON
 
@@ -69,79 +68,40 @@ function save_results(calib_problem, calib_result, jld_file, json_file)
     end
 end
 
+instr_calib_filename = "instr_$(proj_info.msinstrument)_$(proj_info.quanttype)_$(proj_info.quantobj)_calib_$(proj_info.id)_$(proj_info.data_ver)"
 res_jld_file = joinpath(scratch_path, "$(instr_calib_filename)_borg.jld2")
 res_json_file = joinpath(scratch_path, "$(instr_calib_filename)_borg.json")
 
 ini_population = Matrix{Float64}(undef, 0, 0)
 
-MaxTime = 3600.0
+MaxTime = 600.0
 min_ini_sigma = 1E-2
 ini_sigma = 1.0
 ini_lnB = nothing
 
 using BlackBoxOptim
 
-@info "Running Borg..."
-borg_opt = bbsetup(mscalib_data; PopulationSize=200,
+@info "Noise Model Calibration..."
+bbox_opt = bbsetup(mscalib_data; PopulationSize=1000,
                    MaxTime=MaxTime, MaxSteps=10^8, NThreads=Threads.nthreads()-1,
-                   MaxStepsWithoutProgress=50000,
+                   #MaxStepsWithoutProgress=50000,
                    TraceMode=:verbose, TraceInterval=5.0,
-                               ϵ=[1E-7, 1E-7], Population=ini_population);
-borg_res = bboptimize(borg_opt)
-ini_population = BBOUtils.popmatrix(borg_res)
-@info "Done Borg..."
-save_results(BlackBoxOptim.problem(borg_opt), borg_res, res_jld_file, res_json_file)
+                   #fitness_scheme=ScalarFitnessScheme{false}(),
+                   detectionMaxMin=0.98,
+                   ϵ=[1E-2, 1E-1], Population=ini_population);
+bbox_res = bboptimize(bbox_opt)
+ini_population = BBOUtils.popmatrix(bbox_res)
+@info "Done Calibration..."
+save_results(BlackBoxOptim.problem(bbox_opt), bbox_res, res_jld_file, res_json_file)
 
 @info "Done"
 
-using PlotlyJS
-instr_calib_model = MSInstrumentCalibration.params2model(BlackBoxOptim.problem(borg_opt).factory, best_candidate(borg_res))
+include(joinpath(misc_scripts_path, "plots", "plotly_utils.jl"))
 
-log2_intensity_range = 12:0.01:35.0
-intensity_range = 2 .^ log2_intensity_range
-plot([scatter(x = intensity_range, name="rel_sd",
-              y = MSInstrument.signal_std.(Ref(instr_calib_model), intensity_range) ./ intensity_range),
-      scatter(x = intensity_range, y = fill(1.0, length(intensity_range)), name="1", mode=:lines, line_dash=:dash)],
-     Layout(xaxis=attr(type="log"), yaxis=attr(type="log")))
-plot([scatter(x = intensity_range, name="sd", legendgroup="sd", showlegend=true, line_color=:blue,
-              y = intensity_range .+ MSInstrument.signal_std.(Ref(instr_calib_model), intensity_range)),
-      scatter(x = intensity_range, name="sd", legendgroup="sd", showlegend=false, line_color=:blue,
-              y = intensity_range .- MSInstrument.signal_std.(Ref(instr_calib_model), intensity_range)),
-      scatter(x = intensity_range, y = intensity_range, name="I", mode=:lines, line_color=:orangered, line_dash=:dash),
-      scatter(x = intensity_range, y = 1.5intensity_range, name="I+I", mode=:lines, line_color=:orangered, line_dash=:dot),
-      scatter(x = intensity_range, y = 0.66intensity_range, name="I+I", mode=:lines, line_color=:orangered, line_dash=:dot)],
-      Layout(xaxis=attr(type="log"), yaxis=attr(type="log")))
+@load("/pool/analysis/astukalov/cov2/scratch/instr_QEP5_intensity_pepmodstate_calib_cov2_20200425_borg.jld2", calib_model)
+instr_calib_model = calib_model
+instr_calib_model = MSInstrumentCalibration.params2model(BlackBoxOptim.problem(bbox_opt).factory, best_candidate(bbox_res))
 
-plot([scatter(x = intensity_range, name="detected", line_color=:red,
-              y = exp.(MSInstrument.detection_likelihood_log.(Ref(instr_calib_model), true, log.(intensity_range)))),
-      scatter(x = intensity_range, name="missed", line_color=:blue,
-              y = exp.(MSInstrument.detection_likelihood_log.(Ref(instr_calib_model), false, log.(intensity_range))))],
-    Layout(xaxis=attr(type="log")))
-
-plot_intensities_df = @where(instr_calib_data.intensities_df, !isnull(:expected_log) & !isnull(:error));
-plot_intensities_df[:log_error] = log10.(abs.(dropnull(plot_intensities_df[:error])));
-scatter(dropnull(plot_intensities_df[:expected_log]),
-        dropnull(plot_intensities_df[:log_error]))
-
-#=
-using Cairo, Gadfly
-
-draw(PDF( joinpath(scratch_path,"$(instr_calib_filename)_intensities.pdf"), 16inch, 12inch),
-plot( instr_calib_data.intensities_df, x="expected_log", y = "error",
-      Geom.point ) )
-
-PCP.signal_precision(instrument, 40.0)
-
-draw(PDF( joinpath(scratch_path,"$(instr_calib_filename)_std.pdf"), 16inch, 12inch),
-plot( [x->x, x -> PCP.signal_std(instrument, x)],
-#plot( [x->x, x -> x + 1.0/PCP.signal_precision(instrument, x), x -> x - 1.0/PCP.signal_precision(instrument, x) ],
-      1,1E+7 ) )
-
-names(instr_calib_jlser["features"])
-
-keys(instr_calib_jlser)
-
-using ProfileView
-
-ProfileView.view()
-=#
+PlotlyUtils.MSInstrument.plot_rel_sd(instr_calib_model, intensity_range = 2 .^ (15:0.01:35))
+PlotlyUtils.MSInstrument.plot_sd(instr_calib_model, intensity_range = 2 .^ (15:0.01:35))
+PlotlyUtils.MSInstrument.plot_detection(instr_calib_model, intensity_range = 2 .^ (15:0.01:35))
--- qx7_dia_proteome_calib.jl
+++ qx7_dia_proteome_calib.jl
@@ -1,14 +1,12 @@
 proj_info = (id = "cov2",
-             data_ver = "20200423",
+             data_ver = "20200428",
              fit_ver = "20200428",
-             modelobj = :ptmgroup,
+             modelobj = :protgroup,
              msinstrument = "QX7",
-             quantobj = :ptmgroup,
+             quantobj = :protgroup,
              quanttype = :intensity,
              quantcol = :intensity,
-             ms_folder = "phospho_qx7_calib_20200423",
-             #ms_folder = "cov2timecourse_phospho_dia_20200423"
-             )
+             ms_folder = "cov2timecourse_dia_20200423")
 using Pkg
 Pkg.activate(@__DIR__)
 using Revise
@@ -22,7 +20,7 @@ const plots_path = joinpath(analysis_path, "plots")
 
 @info "Calibrating instrument $(proj_info.msinstrument) for $(proj_info.id) project (data v$(proj_info.data_ver), fit v$(proj_info.fit_ver))"
 
-Revise.includet(joinpath(misc_scripts_path, "bbo_utils.jl"))
+include(joinpath(misc_scripts_path, "bbo_utils.jl"))
 include(joinpath(misc_scripts_path, "msinstrument.jl"))
 Revise.includet(joinpath(misc_scripts_path, "msinstrument_calib.jl"))
 include(joinpath(misc_scripts_path, "frame_utils.jl"))
@@ -32,40 +30,29 @@ Revise.includet(joinpath(misc_scripts_path, "spectronaut_utils.jl"));
 data_path = joinpath(base_analysis_path, proj_info.id, "data")
 scratch_path = joinpath(base_analysis_path, proj_info.id, "scratch")
 
-calib_dsname = "20200417_QX7_MaTa_SA_A549"
-calib_filename = "COV2_DIA_phospho_control samples.txt"
-#calib_filename = "COV2_DIA_phospho_0.75probablity_no normalization.txt"
-calib_data_df = CSV.read(joinpath(data_path, proj_info.ms_folder, calib_filename),
-                         delim='\t', truestrings=["True"], falsestrings=["False"])
-msruns_df = DataFrame(report_colname = names(calib_data_df)[occursin.(calib_dsname, string.(names(calib_data_df)))])
-msruns_df.rawfile = string.(msruns_df.report_colname)
-rawfilename_matches = match.(Ref(Regex(string("^", calib_dsname, "_(?<msrun>(?<condition>phospho_test)_(?<replicate>\\d+))\$"))),
-       msruns_df.rawfile)
-#rawfilename_matches = match.(Ref(Regex(string("^", calib_dsname, "_(?<msrun>(?<condition>(?:SARS_COV2|mock)_\\d+h(?:pi)?)_(?<replicate>\\d+))\$"))),
-#       msruns_df.rawfile)
+protgroup_data, report_colinfo = SpectronautUtils.read_proteins_report(joinpath(data_path, proj_info.ms_folder, "COVID proteome NO normalization_Report.txt"),
+                import_data=[:quantity], delim='\t')
+quantity_colinfo_df = SpectronautUtils.metrics_colinfo(report_colinfo[:quantity])
+msruns_df = unique!(select(quantity_colinfo_df, [:msrun_ix, :rawfile]))
+calib_dsname = "20200418_QX7_MaTa_SA_proteome_A549"
+rawfilename_matches = match.(Ref(Regex(string("^", calib_dsname, "_(?<msrun>(?<condition>.+)_(?<replicate>\\d+))(?:_\\d+)?\\.raw\$"))),
+       levels(msruns_df.rawfile))
 msruns_df.msrun = string.(getindex.(rawfilename_matches, :msrun))
 msruns_df.replicate = parse.(Int, getindex.(rawfilename_matches, :replicate))
 msruns_df.condition = string.(getindex.(rawfilename_matches, :condition))
 msruns_df[!, :mstag] .= "F"
-ptms_df = select(calib_data_df, Not(msruns_df.report_colname))
-rename!(n -> replace(replace(string(n), r"^[NT]:\s" => ""), "." => "_"), ptms_df)
-ptms_df.use_for_calib = .!ptms_df.EG_IsDecoy
-ptms_df.ptmgroup_id = categorical(ptms_df.PTM_collapse_key)
-calib_data_df.ptmgroup_id = copy(ptms_df.ptmgroup_id)
-ptmgroup_intensities_df = FrameUtils.pivot_longer(calib_data_df, :ptmgroup_id,
-                        measure_vars_regex=Regex("^(?<var>$(calib_dsname)\\w+)\$"),
-                        var_col=:rawfile, value_col=:log2_intensity)
-filter!(r -> isfinite(r.log2_intensity), ptmgroup_intensities_df)
-ptmgroup_intensities_df.intensity = 2 .^ ptmgroup_intensities_df.log2_intensity
-ptmgroup_intensities_df = join(ptmgroup_intensities_df, select(msruns_df, [:report_colname, :msrun, :replicate, :condition, :mstag]),
-                          on=:rawfile=>:report_colname, kind=:left)
-select!(ptmgroup_intensities_df, Not(:rawfile))
-ptm_used_intensities_df = ptmgroup_intensities_df[ptmgroup_intensities_df.ptmgroup_id .∈ Ref(Set(sample(ptms_df[ptms_df.use_for_calib, :ptmgroup_id], 3000))), :]
-mscalib_data = MSInstrumentCalibration.MSErrorCalibrationData(ptm_used_intensities_df, ptms_df, msruns_df,
-        object_col=:ptmgroup_id, mschannel_col=:msrun, exp_col=:condition, tech_repl_col=:replicate,
-        error_scale=0.8) # assume 80% of variation comes from measurement
+protgroups_df = select(protgroup_data, report_colinfo[:protgroup])
+protgroups_df.is_contaminant = occursin.(Ref(r"(?:^|;)CON__"), protgroups_df.protgroup_sn_id)
+protgroups_df.use_for_calib = .!protgroups_df.is_contaminant
 
-instr_calib_filename = "instr_$(proj_info.msinstrument)_$(proj_info.quanttype)_$(proj_info.quantobj)_calib_$(proj_info.id)_$(proj_info.fit_ver)"
+pg_intensities_df = FrameUtils.pivot_longer(protgroup_data, :protgroup_id,
+                        measure_vars_regex=r"^(?<value>[^.]+)\.\[(?<var>\d+)\]\s(?<rawfile>.+)?$",
+                        var_col=:msrun_ix, value_col=:metric)
+pg_intensities_df[!, :msrun_ix] = parse.(Int, string.(pg_intensities_df[!, :msrun_ix]))
+pg_used_intensities_df = pg_intensities_df[pg_intensities_df.protgroup_id .∈ Ref(Set(sample(protgroups_df[protgroups_df.use_for_calib, :protgroup_id], 3000))), :]
+mscalib_data = MSInstrumentCalibration.MSErrorCalibrationData(pg_used_intensities_df, protgroups_df, msruns_df,
+        mschannel_col=:msrun_ix, exp_col=:condition, tech_repl_col=:replicate,
+        error_scale=0.8) # assume 80% of variation comes from measurement
 
 using JLD2, JSON
 
@@ -85,15 +72,11 @@ function save_results(calib_problem, calib_result, jld_file, json_file)
     end
 end
 
-@load("/pool/analysis/astukalov/cov2/scratch/instr_QX7_intensity_ptmgroup_calib_cov2_20200423_borg.jld2", calib_model)
-calib_model
-
+instr_calib_filename = "instr_$(proj_info.msinstrument)_$(proj_info.quanttype)_$(proj_info.quantobj)_calib_$(proj_info.id)_$(proj_info.data_ver)"
 res_jld_file = joinpath(scratch_path, "$(instr_calib_filename)_borg.jld2")
 res_json_file = joinpath(scratch_path, "$(instr_calib_filename)_borg.json")
-instr_calib_model = calib_model
 
 ini_population = Matrix{Float64}(undef, 0, 0)
-
 MaxTime = 600.0
 min_ini_sigma = 1E-2
 ini_sigma = 1.0
@@ -102,7 +85,7 @@ ini_lnB = nothing
 using BlackBoxOptim
 
 @info "Noise Model Calibration..."
-bbox_opt = bbsetup(mscalib_data; PopulationSize=500,
+bbox_opt = bbsetup(mscalib_data; PopulationSize=300,
                    MaxTime=MaxTime, MaxSteps=10^8, #NThreads=Threads.nthreads()-1,
                    MaxStepsWithoutProgress=50000,
                    TraceMode=:verbose, TraceInterval=5.0,
